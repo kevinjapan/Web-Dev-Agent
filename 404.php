86eb6225b25224fc9b076ce1e1e550a066879a3f
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2023-06-28 20:37:12 +0100

    Create 404 page

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php
+get_header();
+?>
+
+<section class="fade_in" style="height:50vh;padding-top:2rem;">
+	<h4 class=""><?php esc_html_e( 'Page not found.', 'wda' ); ?></h4>
+	<p><?php esc_html_e( 'Sorry, the page you requested was not found.', 'wda' ); ?></p>
+</section>
+
+<?php
+get_footer();
