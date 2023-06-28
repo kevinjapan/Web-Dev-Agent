e7a95dbb747c4a685769ccbb64ef2ccc5e8d6e40
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2023-06-28 21:14:21 +0100

    next and prev post links

--- single-wda_package.php
+++ single-wda_package.php
@@ -21,5 +21,10 @@
           
    <?php endwhile; ?>
 
-   
+              
+   <div style="display:flex;gap:2rem;">
+      <div><?php next_post_link('&laquo; %link', '%title' ); ?></div>
+      <div><?php previous_post_link( ' %link &raquo;', '%title' ); ?></div>
+   </div>
+ 
 <?php get_footer(); ?>
