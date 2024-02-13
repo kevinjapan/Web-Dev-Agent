5144a69abc48a60bbca28530a964ce9aba5e6c75
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-02-13 10:54:49 +0000

    update index.php as default posts page

--- index.php
+++ index.php
@@ -1,21 +1,65 @@
+<?php
+/*
+Our Blog (default Posts) Page
 
-<!-- index.php
-template tags:
-get_header() ;
-get_footer() ;
-get_sidebar() ;
-get_sidebar('snarfer') ;
+Unexpected, but when a static front page is used (as is default w/ Web Dev Agent) and the 
+site has any page defined for the blog list (eg a page called 'Blog'), home.php is used as
+the designated blog list page! (Or index.php if home.php doesn't exist).
+So, we effectively add a stub page called 'Blog' and add this to the menu, but wordpress 
+internals through Admin/Settings/Reading/A Static Page will re-direct to this page.
+*/
 
-custom templates:
-get_template_part();
-get_template_part( 'content', 'product' );
-get_template_part( 'content-templates/content', 'product' );
-get_template_part( 'content-templates/content', 'profile' );
+?>
+<?php get_header(); ?>
 
-theme directory:
-get_theme_file_uri() ;
-get_theme_file_path() ;
-get_permalink();
- -->
 
+<h1 class="fade_in"><?php single_cat_title();?></h1>
+
+<section class="feature_tiles stagger_tiles fade_in">
+   <ul>
+   <?php 
+      if(have_posts()) {
+         while(have_posts()) {
+            the_post();?>
+            <li class="news_tile" style="border:none;">
+               <?php if(has_post_thumbnail()):?>
+                  <img src="<?php the_post_thumbnail_url('large'); ?>"/>
+               <?php endif;?>
+               <h4><?php the_title();?></h4>
+               <?php the_excerpt();?>
+               <div class="wp-block-button te_button">
+                  <a class="wp-block-button__link wp-element-button" href="<?php the_permalink(); ?>">read more</a>
+               </div>
+            </li>
+            <?php
+         } 
+      }
+   ?>
+   </ul>
+</section>
+      
+<section class="feature_tiles stagger_tiles fade_in">
+   <ul>
+      <?php 
+      if(have_posts()) :
+         while(have_posts()) :
+            the_post();?>   
+                  <li>
+                     <?php if(has_post_thumbnail()):?>
+                        <img src="<?php the_post_thumbnail_url('medium'); ?>"/>
+                     <?php endif;?>
+                     <h5><?php echo the_title();?></h5>
+                     <p><?php echo the_excerpt();?></p>
+                     <button><a href="<?php the_permalink(); ?>">read more</a></button>
+                  </li>
+            <?php
+         endwhile; 
+      endif;
+      ?>
+   </ul>
+</section>
+
+<!-- wp_link_pages() -->
+
+<?php get_footer(); ?>
 
