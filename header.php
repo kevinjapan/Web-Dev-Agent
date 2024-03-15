6ba4beffd21f11708da764346bac4ad55cb1888b
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-03-15 14:44:32 +0000

    support mobile device scale

--- header.php
+++ header.php
@@ -8,6 +8,7 @@ Global Page Header
 
 <head>
    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+   <meta name="viewport" content="width=device-width, initial-scale=1">
    <title><?php echo get_bloginfo('name');?></title>
    <?php wp_head(); ?>
 </head>
