482363cd01f3c130b20c245ce35bf3284b39aee3
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2025-03-03 11:26:38 +0000

    rename -> wda_discrete_buttons

--- inc/block-pattern-templates/wda-button-blocks-templates.php
+++ inc/block-pattern-templates/wda-button-blocks-templates.php
@@ -9,17 +9,17 @@ function wda_register_button_blocks_templates($site_uri) {
 
 
    // Single Button Block
-   // we contain even a single button in a wp-block-buttons group to facilitate additional buttons
+   // we contain even a single button in a wp-block-buttons group to facilitate adding buttons
    register_block_pattern(
       'wda-button',
       [
          'title' => __('Single Button Block', 'wda'),
          'description' => _x( 'Single Button.', 'A single Button Block.', 'wda' ),            
          'keywords' => ['button'],
-         'categories' => ['wda-buttons'],
+         'categories' => ['wda-buttons-blocks'],
          'viewportWidth' => 1000,
-         'content' =>  '<!-- wp:buttons  {"className":"wda-buttons"} -->
-                        <div class="wp-block-buttons wda_buttons wda_cover_btns">
+         'content' =>  '<!-- wp:buttons  {"className":"wda_discrete_buttons"} -->
+                        <div class="wp-block-buttons wda_discrete_buttons wda_cover_btns">
                            <!-- wp:button -->
                            <div class="wp-block-button wda_button wda_cover_btn"><a class="wp-block-button__link">read more</a></div>
                            <!-- /wp:button -->
@@ -34,10 +34,10 @@ function wda_register_button_blocks_templates($site_uri) {
          'title' => __('Button Group Block', 'wda'),
          'description' => _x( 'Button Group.', 'A Button Group Block.', 'wda' ),            
          'keywords' => ['button'],
-         'categories' => ['wda-buttons'],
+         'categories' => ['wda-buttons-blocks'],
          'viewportWidth' => 1000,
-         'content' =>  '<!-- wp:buttons  {"className":"wda-buttons"} -->
-                        <div class="wp-block-buttons wda_buttons wda_cover_btns">
+         'content' =>  '<!-- wp:buttons  {"className":"wda_discrete_buttons"} -->
+                        <div class="wp-block-buttons wda_discrete_buttons wda_cover_btns">
                            <!-- wp:button -->
                            <div class="wp-block-button wda_button wda_cover_btn"><a class="wp-block-button__link">read more</a></div>
                            <!-- /wp:button -->
